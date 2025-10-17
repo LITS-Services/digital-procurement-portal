24bead231e508af06ed8e1311b56fd977a2f9152
sahalsultan <sahal.sultan@lits.services>
2025-10-17 17:21:57 +0500

    added modals on row click for detailed information of exception and http logs
    added modals on row click for detailed information of exception and http logs

--- src/app/configuration/logs/logs.component.ts
+++ src/app/configuration/logs/logs.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ColumnMode, SelectionType } from '@swimlane/ngx-datatable';
 import { SystemService } from 'app/shared/services/system.service';
 
@@ -14,7 +15,6 @@ export class LogsComponent implements OnInit {
   public SelectionType = SelectionType;
   public ColumnMode = ColumnMode;
 
-  // Shared state
   chkBoxSelected: any[] = [];
   loading = false;
   logsData: any[] = [];
@@ -25,9 +25,12 @@ export class LogsComponent implements OnInit {
   totalItems = 0;
 
   selectedTab: LogType = 'exception'; // default tab
+  selectedLog: any = null;
 
+  @ViewChild('logDetailModal') logDetailModal!: TemplateRef<any>;
   constructor(private systemService: SystemService,
-              private cdr: ChangeDetectorRef) { }
+    private cdr: ChangeDetectorRef,
+    private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.loadLogs();
@@ -42,57 +45,21 @@ export class LogsComponent implements OnInit {
     }
   }
 
-  // loadLogs() {
-  //   this.loading = true;
-
-  //   if (this.selectedTab === 'exception') {
-  //     this.columns = [
-  //       { name: 'Sr. No.', cellTemplate: 'srNoTemplate', width: 50 },
-  //       { name: 'TimeStamp', prop: 'timestamp' },
-  //       { name: 'Exception Type', prop: 'exceptionType' },
-  //       { name: 'Message', prop: 'message' },
-  //       { name: 'Http Method', prop: 'httpMethod' },
-  //       { name: 'Body', prop: 'body' }
-  //     ];
-
-  //     this.systemService.getAllExceptionLogs(this.currentPage, this.pageSize).subscribe({
-  //       next: (data: any) => this.handleResponse(data),
-  //       error: () => this.loading = false
-  //     });
-  //   } else if (this.selectedTab === 'http') {
-  //     this.columns = [
-  //       { name: 'Sr. No.', cellTemplate: 'srNoTemplate', width: 50 },
-  //       { name: 'TimeStamp', prop: 'timestamp' },
-  //       { name: 'Http Method', prop: 'httpMethod' },
-  //       { name: 'Url', prop: 'url' },
-  //       { name: 'Request Body', prop: 'requestBody', sortable: false },
-  //       { name: 'Response Body', prop: 'responseBody' },
-  //       { name: 'Status', prop: 'status' }
-  //     ];
-
-  //     this.systemService.getAllHttpLogs(this.currentPage, this.pageSize).subscribe({
-  //       next: (data: any) => this.handleResponse(data),
-  //       error: () => this.loading = false
-  //     });
-  //   }
-  // }
-
   loadLogs() {
-  this.loading = true;
+    this.loading = true;
 
-  if (this.selectedTab === 'exception') {
-    this.systemService.getAllExceptionLogs(this.currentPage, this.pageSize).subscribe({
-      next: (data: any) => this.handleResponse(data),
-      error: () => this.loading = false
-    });
-  } else if (this.selectedTab === 'http') {
-    this.systemService.getAllHttpLogs(this.currentPage, this.pageSize).subscribe({
-      next: (data: any) => this.handleResponse(data),
-      error: () => this.loading = false
-    });
+    if (this.selectedTab === 'exception') {
+      this.systemService.getAllExceptionLogs(this.currentPage, this.pageSize).subscribe({
+        next: (data: any) => this.handleResponse(data),
+        error: () => this.loading = false
+      });
+    } else if (this.selectedTab === 'http') {
+      this.systemService.getAllHttpLogs(this.currentPage, this.pageSize).subscribe({
+        next: (data: any) => this.handleResponse(data),
+        error: () => this.loading = false
+      });
+    }
   }
-}
-
 
   handleResponse(data: any) {
     this.logsData = data?.result || [];
@@ -107,4 +74,18 @@ export class LogsComponent implements OnInit {
     this.loadLogs();
   }
 
+  onRowClick(event: any) {
+    console.log('Row clicked:', event);
+
+    this.selectedLog = event.row;
+    this.modalService.open(this.logDetailModal, { size: 'lg', centered: true });
+  }
+
+  onActivate(event: any) {
+    // We only want clicks, not mouseenter or keydown etc.
+    if (event.type === 'click' && event.row) {
+      this.selectedLog = event.row;
+      this.modalService.open(this.logDetailModal, { size: 'lg', centered: true });
+    }
+  }
 }
