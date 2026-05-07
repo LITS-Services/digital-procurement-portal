7bd61f6d45d80119d2f1f98f0f0d04964ec4e320
alimehdi52 <Ali.Mehdi@lits.services>
2026-05-07 12:55:30 +0500

    Fix: New Purchase Request Draft Issue

--- src/app/purchase-request/new-purchase-request/new-purchase-request.component.ts
+++ src/app/purchase-request/new-purchase-request/new-purchase-request.component.ts
@@ -1096,12 +1096,14 @@ export class NewPurchaseRequestComponent implements OnInit, AfterViewInit, OnDes
       : [];
 
     const payload = {
+      id: this.currentRequestId || null,
       requisitionNo: f.requisitionNo || '',
       submittedDate: f.submittedDate || null,
       deliveryLocation: f.deliveryLocation || '',
       receiverName: f.receiverName || '',
       receiverContact: this.normalizePhoneToString(f.receiverContact),
-      // status: 'Draft',
+      status: 'Draft',
+      requestStatus: 'Draft',
       department: f.department || '',
       designation: f.designation || '',
       businessUnit: f.businessUnit || '',
@@ -1118,7 +1120,14 @@ export class NewPurchaseRequestComponent implements OnInit, AfterViewInit, OnDes
 
     this.loading = true;
     this.spinner.show();
-    const request$ = this.purchaseRequestService.createPurchaseRequest({ purchaseRequest: payload, isDraft: true });
+
+    const request$ =
+      this.currentRequestId
+        ? this.purchaseRequestService.updatePurchaseRequest(this.currentRequestId, {
+          purchaseRequest: payload,
+          isDraft: true
+        })
+        : this.purchaseRequestService.createPurchaseRequest({ purchaseRequest: payload, isDraft: true });
 
     request$.subscribe({
       next: () => this.handleDraftSuccess(),
@@ -1136,6 +1145,7 @@ export class NewPurchaseRequestComponent implements OnInit, AfterViewInit, OnDes
 
   private handleDraftError(err: any) {
     this.loading = false;
+    this.spinner.hide();
     this.toastr.error('Failed to save draft');
     console.error('Error saving draft:', err);
   }
@@ -1195,12 +1205,14 @@ export class NewPurchaseRequestComponent implements OnInit, AfterViewInit, OnDes
       : [];
 
     const payload = {
+      id: this.currentRequestId || null,
       requisitionNo: f.requisitionNo,
       submittedDate: f.submittedDate || null,
       deliveryLocation: f.deliveryLocation,
       receiverName: f.receiverName,
       receiverContact: this.normalizePhoneToString(f.receiverContact),
-      // status: f.status,
+      status: 'New',
+      requestStatus: 'New',
       department: f.department,
       designation: f.designation,
       businessUnit: f.businessUnit,
